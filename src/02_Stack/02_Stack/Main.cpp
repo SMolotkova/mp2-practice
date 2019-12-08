cd388d7a9973a51b5fe5ddc55dca2d8f97c6f306
Svetlana Molotkova <42940205+SMolotkova@users.noreply.github.com>
2019-12-08 21:14:52 +0300

    Update Main.cpp

--- src/02_Stack/02_Stack/Main.cpp
+++ src/02_Stack/02_Stack/Main.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 #include <string>
-#include "header.h"
-#include "stack.h"
-#include "exceptions.h"
+#include "Header.h"
+#include "Stack.h"
+#include "Exceptions.h"
 using namespace std;
 
 void main() 
 {
 	string your_expression;
-	string operands;
-	
-		cout << "\t\t\t\tMY CALCULATOR\n" << "Enter your expression:";
+	cout << "\t\t\t\tMY CALCULATOR\n" << "Enter your expression:";
+	cout << "Enter your expression" << endl;
 		getline(cin, your_expression);
-		string postfix_form = myCalculator::PostfixForm(your_expression);
-		cout << "Postfix form is:" << postfix_form << endl;
-		int count = myCalculator::Count(postfix_form);
-		double* values = new double[count];
-		myCalculator::GettingValues(values, operands, postfix_form, count);
-		double result = myCalculator::Calculate(values, operands, postfix_form);
-		cout << "Result is: " << result << endl;
-	system("pause");
+		try
+		{
+			string postfix_form = myCalculator::PostfixForm(your_expression);
+			cout << "Postfix form is:" << endl << postfix_form << endl;
+			int count = 0;
+			char* operands;
+			double* values;
+			myCalculator::GettingOperands(postfix_form, operands, values, count);
+			double result = myCalculator::Calculate(postfix_form, operands, values, count);
+			cout << "The result is: " << result << endl;
+		}
+		catch (const char* a)
+		{
+			cout << a << endl;
+		}
+		int c = 0;
+		cin >> c;
 }
